1b5261bcd52907653e374cd2636154d1c69499a3
Santiago Guerra <guerrasantiago19@gmail.com>
2020-04-27 16:33:23 -0500

    install chart.js to improve data visualizing

--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -1,6 +1,17 @@
 import { curry } from 'rambda';
+import { createElement } from '../utils';
 
 const Hourly = curry( data => {
+
+
+  const parentElement = document.querySelector('#content');
+
+  const hourly = createElement('<div><h2>Hourly</h2></div>')
+
+  const chart = createElement('<canvas id="hourly-data" width="100%" height="300px"></canvas>')
+
+  console.log(chart)
+
   console.log(data.hourly)
 })
 
